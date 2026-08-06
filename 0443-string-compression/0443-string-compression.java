6424b0a30c4a34bcfc65ded55fbb570718840d83
Nick 0-7 <nikhilkutre77@gmail.com>
2026-08-06 13:19:18 +0530

    Time: 3 ms (10.74%), Space: 45.4 MB (66.13%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb=new StringBuilder("");
+        sb.append(chars[0]);
+        int count=1;
+        for(int i=1;i<chars.length;i++){
+            if(chars[i-1]!=chars[i]){
+                if(count>1){
+                sb.append(count+"");
+                }
+                sb.append(chars[i]);
+                count=1;
+            }else{
+                count++;
+            }
+        }if(count>1){
+            sb.append(count+"");
+        }
+        for(int i=0;i<sb.length();i++){
+            chars[i]=sb.charAt(i);
+        }
+        return sb.length();
+    }
+}
