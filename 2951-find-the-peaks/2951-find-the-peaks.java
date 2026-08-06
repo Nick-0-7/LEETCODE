beb58ec638348d45e292f6c6b3783d530cb7aeb5
Nick 0-7 <nikhilkutre77@gmail.com>
2026-08-06 06:38:32 +0530

    Time: 1 ms (99.66%), Space: 45.5 MB (55.06%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> li=new ArrayList<Integer>();
+        if(mountain.length<3){
+            return li;
+        }else{
+        for(int i=1;i<mountain.length-1;i++){
+            if(mountain[i]>mountain[i-1]&&mountain[i]>mountain[i+1]){
+                li.add(i);
+            }
+        }
+        }
+            return li;
+    }
+}
