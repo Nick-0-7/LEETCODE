c643e9fb086d79e4133faddc513bbbf3979945e0
Nick 0-7 <nikhilkutre77@gmail.com>
2026-08-08 21:25:40 +0530

    Time: 1 ms (89.34%), Space: 43.1 MB (62.26%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-       
-        if (word.equals(word.toUpperCase()) ||
-            word.equals(word.toLowerCase()) ||
-            Character.isUpperCase(word.charAt(0)) &&
-            word.substring(1).equals(word.substring(1).toLowerCase())) {
+       int count=0;
+        for(int i=0;i<word.length();i++){
+         char ch= word.charAt(i);
+           if(ch>='A'&& ch<='Z'){
+              count++;
+           }
+        }
+        if(count==word.length()){
             return true;
         }
-
+        if(count==0){
+            return true;
+        }
+        if(count == 1 && word.charAt(0)>='A'&&  word.charAt(0) <= 'Z'){
+            return true;
+        }
         return false;
-        
     }
 }
